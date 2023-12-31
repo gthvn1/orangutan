7ca853c36eaa9990b28df600845e2dc299aa5924
Guillaume <guillaume.thouvenin@vates.tech>
2023-12-31 13:48:38 +0100

    repl: start the implementation

--- src/main.ml
+++ src/main.ml
@@ -1,1 +1,14 @@
-let () = print_endline "Welcome to Monkey Islang!"
+let rec repl () =
+  print_string ">> ";
+  let line = read_line () in
+  if String.compare "q;" line <> 0 then (
+    print_endline line;
+    repl ())
+  else (
+    print_endline "May your trip be as enjoyable as finding";
+    print_endline "extra bananas at the bottom of the bag!")
+
+let () =
+  print_endline "Welcome to Monkey Islang!";
+  print_endline "Type 'q;' to exit.";
+  repl ()
