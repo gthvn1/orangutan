8ef8bacdc67e41cc92a61d3102bb69d0173651ad
Guillaume <guillaume.thouvenin@vates.tech>
2024-01-02 19:03:50 +0100

    parser: fix an error when updating the parser
    When returning from parsing let statement we need to also return the updated parser.

--- lib/parser.ml
+++ lib/parser.ml
@@ -2,6 +2,8 @@ module A = Ast
 module L = Lexer
 module T = Token
 
+(* a tuple where first element is the current token and the second element is
+   the sequence of remaining tokens *)
 type t = T.t * T.t Seq.t
 
 let new_parser (l : L.t) : t =
@@ -15,24 +17,26 @@ let peek_token (parse : t) : T.t = parse |> snd |> L.tokens_hd
 (* LET STATEMENT  LET <IDENT> = <EXPR> ;                                     *)
 (*****************************************************************************)
 let parse_let_statement_expr (parse : t) (ident : A.identifier) :
-    (A.Statement.t, string) result =
+    (t * A.Statement.t, string) result =
   (* And finally we expect an expression. As we don't parse expresion yet we
      just skip it until getting a semicolon *)
   let rec loop (p : t) : t =
     match fst p with T.Semicolon -> next_token p | _ -> loop (next_token p)
   in
-  let _ = loop parse in
+  let parse = loop parse in
   Ok
-    (A.Statement.Let { token = T.Let; name = ident; value = A.Expression.ToDo })
+    ( parse,
+      A.Statement.Let { token = T.Let; name = ident; value = A.Expression.ToDo }
+    )
 
 let parse_let_statement_assign (parse : t) (ident : A.identifier) :
-    (A.Statement.t, string) result =
+    (t * A.Statement.t, string) result =
   (* Then we are expecting an assignement *)
   match peek_token parse with
   | T.Assign -> parse_let_statement_expr (next_token parse) ident
   | t -> Error ("Expected assign, got " ^ T.to_string t)
 
-let parse_let_statement (parse : t) : (A.Statement.t, string) result =
+let parse_let_statement (parse : t) : (t * A.Statement.t, string) result =
   (* First we are expecting an Identifier *)
   match peek_token parse with
   | T.Ident value as token ->
@@ -56,8 +60,7 @@ let parse_program (parse : t) : A.Program.t =
       match parsed_stmt with
       | Error e ->
           parse_stmts (next_token parse) { prog with errors = e :: prog.errors }
-      | Ok s ->
-          parse_stmts (next_token parse)
-            { stmts = s :: prog.stmts; errors = prog.errors }
+      | Ok (p, s) ->
+          parse_stmts p { stmts = s :: prog.stmts; errors = prog.errors }
   in
   parse_stmts parse prog
