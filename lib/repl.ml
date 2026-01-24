3ba9edb22bbe16eb4aa5729ae03c0adc5ff69fd6
Guillaume <gthvn1@gmail.com>
2026-01-24 21:25:22 +0100

    Add test

--- lib/repl.ml
+++ lib/repl.ml
@@ -1,1 +1,4 @@
+(** [add x y] just adds two numbers. It is to check with alcotest. *)
+let add x y = x + y
+
 let hello () = print_endline "Jello from REPL"
---
+++ test/test_monkey.ml
@@ -0,0 +1,6 @@
+open Alcotest
+
+let test_add () = check int "same int" 3 (Monkey.Repl.add 1 2)
+
+let () =
+  run "Monkey test" [ ("add", [ test_case "1 + 2 = 3" `Quick test_add ]) ]
