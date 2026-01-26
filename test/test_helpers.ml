0def5246b7541660840091f6a34c207a814479ef
Guillaume <gthvn1@gmail.com>
2026-01-26 07:08:44 +0100

    Split tests

---
+++ test/test_helpers.ml
@@ -0,0 +1,16 @@
+let pp_token_type fmt tt =
+  Format.fprintf fmt "%s" (Monkey.Token.string_of_token_type tt)
+
+let token_type = Alcotest.testable pp_token_type ( = )
+
+let check_tokens lexer expected =
+  let rec aux l e =
+    let tok, next_lexer = Monkey.Lexer.next_token l in
+    match e with
+    | [] -> ()
+    | (tt, lit) :: xs ->
+        Alcotest.check token_type "same token type" tt tok.ty;
+        Alcotest.check Alcotest.string "same literal" lit tok.literal;
+        aux next_lexer xs
+  in
+  aux lexer expected
