7f8bd4ba7b2fe75aadaf5b532530ac628fb6fb49
Guillaume <guillaume.thouvenin@vates.tech>
2026-01-26 15:08:29 +0100

    Start thinking how to implement structure

--- lib/parser.ml
+++ lib/parser.ml
@@ -1,1 +1,25 @@
 let ping () = "pong"
+
+module Ast = struct
+  type identifier = Token.t * string
+  type expression = unit (* We don't know yet *)
+  type statement = Let of Token.t * identifier * expression
+  type program = statement list
+  type node = I of identifier | E of expression | S of statement
+
+  (** [token_literal node] returns the literal value of the token associated to
+      the [node]. It is used for debugging and testing. *)
+  let token_literal node =
+    match node with
+    | I _ -> print_endline "TODO: implement token_literal for identifier"
+    | S _ -> print_endline "TODO: implement token_literal for statement"
+    | E _ -> print_endline "TODO: implement token_literal for expression"
+end
+
+(* Let statement:
+  - A statement doesn't produce a result (<> from statement)
+  - Let statement has three parts:
+    - a "Let" keyword
+    - an identifier
+    - an expression
+  *)
