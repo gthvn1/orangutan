d4b97e5ca29def6be878befce72e82597a8dac95
Guillaume <guillaume.thouvenin@vates.tech>
2026-01-26 14:48:41 +0100

    modify output of test name

--- test/test_monkey.ml
+++ test/test_monkey.ml
@@ -1,4 +1,4 @@
 let () =
   let open Alcotest in
   run "Monkey test"
-    [ ("test lexer", Test_lexer.tests); ("test parser", Test_parser.tests) ]
+    [ ("Lexer", Test_lexer.tests); ("Parser", Test_parser.tests) ]
