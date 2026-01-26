3ad01763245a4401c3974f3af0565ab2fb20c341
Guillaume <gthvn1@gmail.com>
2026-01-26 21:16:46 +0100

    Check identifier in the test

--- test/test_parser.ml
+++ test/test_parser.ml
@@ -10,10 +10,19 @@ let test_empty_statement () =
 let test_let_statements () =
   let open Monkey in
   let input = "let x = 5; let y = 10; let foobar = 838383;" in
+  let expected_ident = [ "x"; "y"; "foobar" ] in
   let lexer = Lexer.create input in
   let parser = Parser.create lexer in
   let program = Parser.parse_program parser in
-  check int "number of statements" 3 (List.length program)
+  check int "number of statements" 3 (List.length program);
+  let lst = List.combine expected_ident program in
+  List.iter
+    (fun ((e : string), (s : Ast.Statement.t)) ->
+      match s with
+      | Let (l : Ast.Statement.let_statement) ->
+          check string "token" "let" (Ast.Statement.token_literal s);
+          check string "identifier" e l.name.value)
+    lst
 
 let tests =
   [
