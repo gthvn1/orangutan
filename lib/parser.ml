7c7e0e3be7ba34a6ef08da6fe5ddcbcbc1fa5f57
Guillaume <guillaume.thouvenin@vates.tech>
2026-01-26 17:56:20 +0100

    Continue to think about how to represent AST

--- lib/parser.ml
+++ lib/parser.ml
@@ -1,25 +1,45 @@
 let ping () = "pong"
 
 module Ast = struct
-  type identifier = Token.t * string
-  type expression = unit (* We don't know yet *)
-  type statement = Let of Token.t * identifier * expression
-  type program = statement list
-  type node = I of identifier | E of expression | S of statement
+  type identifier = { token : Token.t; value : string }
+  (** [identifier] represents a variable name in Monkey. It consists of the
+      token associated with the identifier (used for debugging/testing) and the
+      string value of the identifier itself. *)
 
-  (** [token_literal node] returns the literal value of the token associated to
-      the [node]. It is used for debugging and testing. *)
-  let token_literal node =
+  (** [e] represents expressions in Monkey. For now, the only expression is an
+      identifier. Later, this will be extended to include literals, function
+      calls, infix expressions, etc. *)
+  type e = Ident of identifier
+
+  type let_statement = { token : Token.t; name : identifier; value : e }
+  (** [let_statement] represents a 'let' statement in Monkey. It binds an
+      expression to a variable name.
+
+      Fields:
+      - [token]: the 'let' token itself (used for token_literal, debugging)
+      - [name]: the identifier being bound
+      - [value]: the expression assigned to the identifier *)
+
+  (** [s] represents all statements in Monkey. Currently, the only statement
+      type is 'let_statement', but more can be added later. *)
+  type s = Let of let_statement
+
+  type program = s list
+  (** [program] is the root of every AST. It is simply a list of statements,
+      representing the Monkey program. *)
+
+  (** [node] is a wrapper type to allow a single function (like [token_literal])
+      to operate on both expressions and statements.
+
+      - [Expression e]: wraps an expression
+      - [Statement s]: wraps a statement *)
+  type node = Expression of e | Statement of s
+
+  (** [token_literal node] returns the literal value of the token associated
+      with the node. This is primarily used for debugging and testing, not for
+      evaluation. *)
+  let token_literal node : string =
     match node with
-    | I _ -> print_endline "TODO: implement token_literal for identifier"
-    | S _ -> print_endline "TODO: implement token_literal for statement"
-    | E _ -> print_endline "TODO: implement token_literal for expression"
+    | Statement _ -> failwith "TODO: implement token_literal for statement"
+    | Expression _ -> failwith "TODO: implement token_literal for expression"
 end
-
-(* Let statement:
-  - A statement doesn't produce a result (<> from statement)
-  - Let statement has three parts:
-    - a "Let" keyword
-    - an identifier
-    - an expression
-  *)
