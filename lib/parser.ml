617384786985024f236c4d6b67c1e826079ae3a7
Guillaume <gthvn1@gmail.com>
2026-01-27 20:48:48 +0100

    Return errors as a list of strings

--- lib/parser.ml
+++ lib/parser.ml
@@ -3,15 +3,18 @@ let debug = ref true
 
 module Stmt = Ast.Statement
 
-type t = { lexer : Lexer.t; cur_token : Token.t; peek_token : Token.t }
+type t = {
+    lexer : Lexer.t
+  ; cur_token : Token.t
+  ; peek_token : Token.t
+  ; errors : string list
+}
 (** [debug_lexer label lexer] prints the state of the lexer prepending a
     [label]. *)
 
 type program = Stmt.t list
 (** [program] is the root of every AST. It is simply a list of statements,
     representing the Monkey program. *)
-
-type parse_error = WrongPeekToken | UnkownStatement
 
 let debug_parser (label : string) (parser : t) : unit =
   if !debug then
@@ -25,22 +28,30 @@ let ( let* ) = Result.bind
     have been updated. *)
 let next_token (parser : t) : t =
   let next_token, next_lexer = Lexer.next_token parser.lexer in
-  { lexer = next_lexer; cur_token = parser.peek_token; peek_token = next_token }
+  {
+    parser with
+    lexer = next_lexer
+  ; cur_token = parser.peek_token
+  ; peek_token = next_token
+  }
 
 (** [create lexer] returns a parser initialized where current token and peek
     token are both set. *)
 let create (lexer : Lexer.t) : t =
   let first_token, lexer = Lexer.next_token lexer in
   let second_token, lexer = Lexer.next_token lexer in
-  { lexer; cur_token = first_token; peek_token = second_token }
+  { lexer; cur_token = first_token; peek_token = second_token; errors = [] }
 
 (** [expect_peek parser token] returns the updated parser and true if [token]
     matches the peek token. Otherwise parser is not updated and false is
     returned. *)
-let expect_peek (parser : t) ~(token : Token.token_type) :
-    (t, parse_error) result =
+let expect_peek (parser : t) ~(token : Token.token_type) : (t, string) result =
   if parser.peek_token.ty = token then Ok (next_token parser)
-  else Error WrongPeekToken
+  else
+    Error
+      (Printf.sprintf "expected next token to be %s, got %s instead"
+         (Token.string_of_token_type token)
+         (Token.string_of_token_type parser.peek_token.ty))
 
 (** [cur_token_is parser token] returns true if the current token of [parser]
     matches [token]. It returns false otherwise. *)
@@ -54,18 +65,19 @@ let peek_token_is (parser : t) ~(token : Token.token_type) : bool =
 
 (** [parse_program parser] is the entry point for parsing tokens. It expects a
     valid [parser] and will return a list of statement. *)
-let rec parse_program (parser : t) : (program, parse_error) result =
+let rec parse_program (parser : t) : program * string list =
   let rec loop prog p =
     match p.cur_token.ty with
-    | Token.Eof -> Ok (List.rev prog)
-    | Token.Let ->
-        let* stmt, p' = parse_let_statement p in
-        loop (stmt :: prog) (next_token p')
-    | _ -> Error UnkownStatement
+    | Token.Eof -> (List.rev prog, List.rev p.errors)
+    | Token.Let -> (
+        match parse_let_statement p with
+        | Ok (stmt, p') -> loop (stmt :: prog) (next_token p')
+        | Error e -> loop prog (next_token { p with errors = e :: p.errors }))
+    | _ -> (List.rev prog, List.rev p.errors)
   in
   loop [] parser
 
-and parse_let_statement (parser : t) : (Stmt.t * t, parse_error) result =
+and parse_let_statement (parser : t) : (Stmt.t * t, string) result =
   debug_parser "[let] begin" parser;
   let stmt_token = parser.cur_token in
 
