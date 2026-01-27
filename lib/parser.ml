408bdcd68c108e3799612eb7db47d1fff70961c1
Guillaume <gthvn1@gmail.com>
2026-01-27 22:14:21 +0100

    Minor refactoring

--- lib/parser.ml
+++ lib/parser.ml
@@ -9,13 +9,13 @@ type t = {
   ; peek_token : Token.t
   ; errors : string list
 }
-(** [debug_lexer label lexer] prints the state of the lexer prepending a
-    [label]. *)
 
 type program = Stmt.t list
 (** [program] is the root of every AST. It is simply a list of statements,
     representing the Monkey program. *)
 
+(** [debug_lexer label lexer] prints the state of the lexer prepending a
+    [label]. *)
 let debug_parser (label : string) (parser : t) : unit =
   if !debug then
     Printf.eprintf "%s current:%s peek:%s\n%!" label
@@ -24,8 +24,8 @@ let debug_parser (label : string) (parser : t) : unit =
 
 let ( let* ) = Result.bind
 
-(** [next_token parser] returns a new parser where current token and peek token
-    have been updated. *)
+(** [next_token parser] returns a new parser where current and peek tokens have
+    been updated. *)
 let next_token (parser : t) : t =
   let next_token, next_lexer = Lexer.next_token parser.lexer in
   {
@@ -38,9 +38,9 @@ let next_token (parser : t) : t =
 (** [create lexer] returns a parser initialized where current token and peek
     token are both set. *)
 let create (lexer : Lexer.t) : t =
-  let first_token, lexer = Lexer.next_token lexer in
-  let second_token, lexer = Lexer.next_token lexer in
-  { lexer; cur_token = first_token; peek_token = second_token; errors = [] }
+  let fst_token, lexer = Lexer.next_token lexer in
+  let snd_token, lexer = Lexer.next_token lexer in
+  { lexer; cur_token = fst_token; peek_token = snd_token; errors = [] }
 
 (** [expect_peek parser token] returns the updated parser and true if [token]
     matches the peek token. Otherwise parser is not updated and false is
@@ -55,30 +55,35 @@ let expect_peek (parser : t) ~(token_type : Token.Type.t) : (t, string) result =
 
 (** [cur_token_type_is parser token] returns true if the current token of
     [parser] matches [token]. It returns false otherwise. *)
-let cur_token_type_is (parser : t) ~(token : Token.Type.t) : bool =
-  parser.cur_token.ty = token
+let cur_token_type_is (parser : t) ~(ty : Token.Type.t) : bool =
+  parser.cur_token.ty = ty
 
 (** [peek_token_type_is parser token] returns true if the peek token of [parser]
     matches [token]. It returns false otherwise. *)
-let peek_token_type_is (parser : t) ~(token : Token.Type.t) : bool =
-  parser.peek_token.ty = token
+let peek_token_type_is (parser : t) ~(ty : Token.Type.t) : bool =
+  parser.peek_token.ty = ty
 
 (** [parse_program parser] is the entry point for parsing tokens. It expects a
     valid [parser] and will return a list of statement. *)
 let rec parse_program (parser : t) : program * string list =
   let open Token.Type in
-  let rec loop prog p =
-    match p.cur_token.ty with
-    | Eof -> (List.rev prog, List.rev p.errors)
-    | Let -> (
-        match parse_let_statement p with
-        | Ok (stmt, p') -> loop (stmt :: prog) (next_token p')
-        | Error e -> loop prog (next_token { p with errors = e :: p.errors }))
-    | Return -> (
-        match parse_return_statement p with
-        | Ok (stmt, p') -> loop (stmt :: prog) (next_token p')
-        | Error e -> loop prog (next_token { p with errors = e :: p.errors }))
-    | _ -> loop prog (next_token p)
+  let consume_stmt parse_fn acc parser =
+    match parse_fn parser with
+    | Ok (s, p) -> (s :: acc, next_token p)
+    | Error e -> (acc, next_token { parser with errors = e :: parser.errors })
+  in
+
+  let rec loop acc parser =
+    if cur_token_type_is parser ~ty:Eof then
+      (List.rev acc, List.rev parser.errors)
+    else
+      let acc', parser' =
+        match parser.cur_token.ty with
+        | Let -> consume_stmt parse_let_statement acc parser
+        | Return -> consume_stmt parse_return_statement acc parser
+        | _ -> (acc, next_token parser)
+      in
+      loop acc' parser'
   in
   loop [] parser
 
@@ -100,7 +105,7 @@ and parse_let_statement (parser : t) : (Stmt.t * t, string) result =
   (* After Assignement we are expecting the expression.
      TODO: parse expression. Until we implement it, advance until SEMICOLON *)
   let rec skip_expression p =
-    if cur_token_type_is p ~token:Token.Type.Semicolon then p
+    if cur_token_type_is p ~ty:Token.Type.Semicolon then p
     else skip_expression (next_token p)
   in
   let parser = skip_expression parser in
@@ -116,7 +121,7 @@ and parse_return_statement (parser : t) : (Stmt.t * t, string) result =
   let parser = next_token parser in
   (* TODO: we are skipping expression until we encounter semi colon *)
   let rec skip_expression p =
-    if cur_token_type_is p ~token:Token.Type.Semicolon then p
+    if cur_token_type_is p ~ty:Token.Type.Semicolon then p
     else skip_expression (next_token p)
   in
   let parser = skip_expression parser in
