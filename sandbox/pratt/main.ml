f2acabf384446f3de41e63e88f58eeda7381099e
Guillaume <gthvn1@gmail.com>
2026-01-29 22:54:42 +0100

    Implement quick tokenizer

--- sandbox/pratt/main.ml
+++ sandbox/pratt/main.ml
@@ -1,3 +1,4 @@
 let () =
+  let _p : Pratt.Token.t = Pratt.Token.Plus in
   Pratt.to_tokens "123" |> ignore;
   print_endline "Hello from pratt"
--- sandbox/pratt/pratt.ml
+++ sandbox/pratt/pratt.ml
@@ -3,4 +3,47 @@
    We support integer, addition, soustraction and multiplication
  *)
 
-let to_tokens (_input : string) = "todo"
+module Token = struct
+  type t = Plus | Minus | Lparen | Rparen | Int of int
+
+  let to_string = function
+    | Plus -> "+"
+    | Minus -> "-"
+    | Lparen -> "("
+    | Rparen -> ")"
+    | Int x -> string_of_int x
+end
+
+let is_digit c = '0' <= c && c <= '9'
+let char_to_int c = Char.code c - Char.code '0'
+
+let to_tokens (input : string) : Token.t list =
+  let digits_to_int (digits : char list) : int =
+    digits |> List.to_seq |> String.of_seq |> int_of_string
+  in
+  let flush_number acc digits =
+    match digits with
+    | [] -> acc
+    | _ ->
+        let n = digits |> List.rev |> digits_to_int in
+        Token.Int n :: acc
+  in
+  let rec loop (num : char list) (acc : Token.t list) (l : char list) =
+    match l with
+    | [] -> flush_number acc num |> List.rev
+    | c :: cs ->
+        if is_digit c then loop (c :: num) acc cs
+        else
+          let token =
+            match c with
+            | '+' -> Token.Plus
+            | '-' -> Token.Minus
+            | '(' -> Token.Lparen
+            | ')' -> Token.Rparen
+            | _ -> failwith (Printf.sprintf "unknown %c" c)
+          in
+          let acc = flush_number acc num in
+          loop [] (token :: acc) cs
+  in
+  let chars = String.to_seq input |> List.of_seq in
+  loop [] [] chars
