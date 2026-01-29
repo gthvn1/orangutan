527b58d9542b11409b221b266248e66d49133afd
Guillaume <guillaume.thouvenin@vates.tech>
2026-01-29 19:16:08 +0100

    Add a sandbox to test things independently of our current project

---
+++ sandbox/pratt/main.ml
@@ -0,0 +1,3 @@
+let () =
+  Pratt.to_tokens "123" |> ignore;
+  print_endline "Hello from pratt"
---
+++ sandbox/pratt/pratt.ml
@@ -0,0 +1,6 @@
+(*
+   We want to parse: +5 - (-2) 
+   We support integer, addition, soustraction and multiplication
+ *)
+
+let to_tokens (_input : string) = "todo"
